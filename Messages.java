c62d0789e1377b351fdb57752b915f38eccb0ddc
ArtLiathain <art.oliathain@gmail.com>
2023-05-16 19:25:42 +0100

    Comit before merge

---
+++ Messages.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class Messages {
+    ArrayList<String> messageStream;
+
+    public Messages(ArrayList<String> messageStream) {
+        this.messageStream = messageStream;
+    }
+
+    
+
+}
