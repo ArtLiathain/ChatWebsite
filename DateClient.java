56da2785405d5ddfe75399584417b03c494efa5b
ArtLiathain <art.oliathain@gmail.com>
2023-05-16 15:54:21 +0100

    Initial commit

---
+++ DateClient.java
@@ -0,0 +1,22 @@
+package Project;
+
+import java.net.*;
+import java.io.*;
+
+public class DateClient implements Runnable {
+    public void run() {
+        try {
+            // this could be changed to an IP name
+            // or address other than the localhost
+            Socket sock = new Socket("127.0.0.1", 6013);
+            InputStream in = sock.getInputStream();
+            BufferedReader bin = new BufferedReader(new InputStreamReader(in));
+            String line;
+            while ((line = bin.readLine()) != null)
+                System.out.println(line);
+            sock.close();
+        } catch (IOException ioe) {
+            System.err.println(ioe);
+        }
+    }
+}
