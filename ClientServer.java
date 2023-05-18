d86644da99baaff6f1a50be75dca1c538e514f17
ArtLiathain <art.oliathain@gmail.com>
2023-05-18 14:35:52 +0100

    Actually working code

--- ClientServer.java
+++ ClientServer.java
@@ -1,55 +1,43 @@
-
 import java.net.*;
 import java.util.ArrayList;
 import java.io.*;
 
 public class ClientServer implements Runnable {
-    ArrayList<String> arrayOfMessages = new ArrayList<String>() {
-    };
 
     public void run() {
+        ServerSocket serverSocket = null;
+        ArrayList<String> testArray = new ArrayList<String>() {
+        };
         try {
-            ServerSocket socketServer = new ServerSocket(6013);
-            // now listen for connections
+            serverSocket = new ServerSocket(1234);
+
             while (true) {
+                System.out.println("Basic stuff");
+                Socket clienSocket = serverSocket.accept();
+                System.out.println("Accepted");
+                InputStreamReader inputStreamReader = new InputStreamReader(clienSocket.getInputStream());
+                System.out.println("got here");
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                OutputStreamWriter outputStreamWriter = new OutputStreamWriter(clienSocket.getOutputStream());
+                BufferedWriter bufferedWriter = new BufferedWriter(outputStreamWriter);
+                System.out.println("even got here");
+                String temp = bufferedReader.readLine();
+                testArray.add(temp);
+                String all = "";
+                for (String string : testArray) {
 
-                System.out.println("Waiting for Clients");
-                Socket inputSocket = socketServer.accept();
-                System.out.println("Clients Accepted");
-                // we have a connection
-
-                // CODE FOR RECEIVING MESSAGE
-                InputStream inputStream = inputSocket.getInputStream();
-                BufferedReader messageReader = new BufferedReader(new InputStreamReader(inputStream));// read message
-                                                                                                      // from the socket
-
-                String message;// assert received message to line variable
-                System.out.println("Read message");
-                // message variable to store line after closing client
-                while ((message = messageReader.readLine()) != null)
-                    arrayOfMessages.add(message); // append line to message
-                System.out.println("Appended To list");
-
-                inputSocket.close(); // close client
-                System.out.println("OutputSocket");
-                Socket outputSocket = socketServer.accept(); // open new client, which should now connect to
-                System.out.println("Output Accepted"); // "ClientReceive.java"
-                PrintWriter printOut = new PrintWriter(
-                        outputSocket.getOutputStream(), true);
-                String allMessages = "";
-                System.out.println("Message Array");
-                for (String oldMessage : arrayOfMessages) {
-                    allMessages += oldMessage;
+                    all += string;
                 }
-                System.out.println("Ans" + allMessages);
-                printOut.println(allMessages);
-                // close the socket and resume listening for more connections
-                outputSocket.close();
+
+                bufferedWriter.write(all);
+                bufferedWriter.newLine();
+                bufferedWriter.flush();
 
             }
-        } catch (IOException ioe) {
-            System.err.println(ioe.toString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
         }
-
     }
+
 }
